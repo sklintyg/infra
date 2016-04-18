1e65e8c1a4b4655e4a5aac9f77339569786c7b0c
Magnus Ekstrand <magnus@ekstrand.net>
2016-04-18 13:37:28 +0200

    REHAB-217: Fix av Sonar rapporterade problem

--- integration/hsa-integration/src/main/java/se/inera/intyg/common/integration/hsa/model/SelectableVardenhet.java
+++ integration/hsa-integration/src/main/java/se/inera/intyg/common/integration/hsa/model/SelectableVardenhet.java
@@ -22,10 +22,11 @@ package se.inera.intyg.common.integration.hsa.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+import java.io.Serializable;
 import java.util.List;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
-public interface SelectableVardenhet {
+public interface SelectableVardenhet extends Serializable {
 
     String getId();
 
