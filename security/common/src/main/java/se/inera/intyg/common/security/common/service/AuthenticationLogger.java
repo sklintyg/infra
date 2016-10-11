3abdc9b5789913c44604b9092dcfccd3d9621169
Karl Johan Andreasson <kalle.kaan@gmail.com>
2016-10-11 15:06:44 +0200

    INTYG-3002: Ursprung för inloggning i händelselogg

--- security/common/src/main/java/se/inera/intyg/common/security/common/service/AuthenticationLogger.java
+++ security/common/src/main/java/se/inera/intyg/common/security/common/service/AuthenticationLogger.java
@@ -22,7 +22,7 @@ package se.inera.intyg.common.security.common.service;
  * Created by eriklupander on 2016-05-18.
  */
 public interface AuthenticationLogger {
-    void logUserLogin(String userHsaId, String authScheme);
+    void logUserLogin(String userHsaId, String authScheme, String origin);
 
     void logUserLogout(String userHsaId, String authScheme);
 
