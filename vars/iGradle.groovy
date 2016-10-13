b0ddcbac653fa78c9fa463b342242cb455ffff66
Jesper Holmberg <jesper.holmberg@callistaenterprise.se>
2016-10-13 14:25:29 +0200

    DEVENV: samlat jenkins-lib 7

---
+++ vars/iGradle.groovy
@@ -0,0 +1,18 @@
+def javaEnv() {
+    def javaHome = tool 'JDK8u66'
+    ["PATH=${env.PATH}:${javaHome}/bin", "JAVA_HOME=${javaHome}"]
+}
+
+def call(gradleCommand) {
+    try {
+        wrap([$class: 'Xvfb']) {
+            withEnv(javaEnv()) {
+                sh gradleCommand
+            }
+        } catch (e) {
+            currentBuild.result = "FAILED"
+            notifyFailed()
+            throw e
+        }
+    }
+}
