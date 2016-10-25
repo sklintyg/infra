29c41032192c511eda9958b5229e229011fb6bce
Erik Lupander <erik.lupander@callistaenterprise.se>
2016-10-25 11:50:41 +0200

    INTYG-3046: Ändrat defaultexpiry för HSA-cacharna till 1 timme

--- integration/hsa-integration/src/main/java/se/inera/intyg/common/integration/hsa/cache/HsaCacheConfiguration.java
+++ integration/hsa-integration/src/main/java/se/inera/intyg/common/integration/hsa/cache/HsaCacheConfiguration.java
@@ -1,18 +1,17 @@
 package se.inera.intyg.common.integration.hsa.cache;
 
+import org.apache.ignite.cache.spring.SpringCacheManager;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import javax.annotation.PostConstruct;
 import javax.cache.expiry.CreatedExpiryPolicy;
 import javax.cache.expiry.Duration;
-
-import org.apache.ignite.cache.spring.SpringCacheManager;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 // import org.springframework.context.annotation.Profile;
 
 /**
  * While the cacheManager.getCache(...) isn't strictly necessary for creating the cache used by
- * {@link se.inera.intyg.webcert.integration.pu.services.PUServiceImpl}, this class provides us with the capability
- * of configuring individual caches based on the current state of the
+ * {@link se.inera.intyg.common.integration.hsa.client.OrganizationUnitServiceBean}, this class
+ * provides us with the capability of configuring individual caches based on the current state of the
  * {@link org.apache.ignite.cache.spring.SpringCacheManager#dynamicCacheCfg}
  *
  * Created by eriklupander on 2016-10-20.
@@ -23,7 +22,7 @@ public class HsaCacheConfiguration {
     public static final String HSA_HEALTH_CARE_UNIT_CACHE_NAME = "hsaHealthCareUnitCache";
     public static final String HSA_HEALTH_CARE_UNIT_MEMBERS_CACHE_NAME = "hsaHealthCareUnitMembersCache";
 
-    private Duration defaultPersonCacheExpiry = Duration.ONE_MINUTE;
+    private Duration defaultPersonCacheExpiry = Duration.ONE_HOUR;
 
     @Autowired
     private SpringCacheManager cacheManager;
