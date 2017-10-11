0d6823997af304378a3ea0141220510eb7f70f72
Per Beijer <per.beijer@usify.se>
2017-10-11 12:51:04 +0200

    INTYG-4667: Lagt in postnummerservice från privatläkarportalen.

---
+++ integration/postnummerservice-integration/src/test/java/se/inera/intyg/infra/integration/postnummer/repository/PostnummerRepositoryFactoryTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2017 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.infra.integration.postnummer.repository;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import se.inera.intyg.infra.integration.postnummer.model.Omrade;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PostnummerRepositoryFactoryTest {
+
+    private PostnummerRepositoryFactory factory = new PostnummerRepositoryFactory();
+
+    private static final String LINE_1 = "13100;NACKA;01;STOCKHOLM;0182;NACKA;01";
+    private static final String LINE_1_POSTNUMMER = "13100";
+    private static final String LINE_1_POSTORT = "NACKA";
+    private static final String LINE_1_LAN = "STOCKHOLM";
+    private static final String LINE_1_KOMMUN = "NACKA";
+
+    @Test
+    public void testCreateOmradeFromString() {
+
+        Omrade res = factory.createOmradeFromString(LINE_1);
+
+        assertNotNull(res);
+        assertEquals(LINE_1_POSTNUMMER, res.getPostnummer());
+        assertEquals(LINE_1_POSTORT, res.getPostort());
+        assertEquals(LINE_1_KOMMUN, res.getKommun());
+        assertEquals(LINE_1_LAN, res.getLan());
+    }
+
+    @Test
+    public void testCreateOmradeWithSetters() {
+        Omrade control = factory.createOmradeFromString(LINE_1);
+        Omrade test = new Omrade(null, null, null, null);
+        test.setKommun(LINE_1_KOMMUN);
+        test.setLan(LINE_1_LAN);
+        test.setPostnummer(LINE_1_POSTNUMMER);
+        test.setPostort(LINE_1_POSTORT);
+        assertTrue(control.hashCode() == test.hashCode());
+    }
+}
---
+++ integration/postnummerservice-integration/src/test/java/se/inera/intyg/infra/integration/postnummer/service/PostnummerServiceTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2017 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.infra.integration.postnummer.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import se.inera.intyg.infra.integration.postnummer.model.Omrade;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = PostnummerServiceTest.TestConfiguration.class)
+public class PostnummerServiceTest {
+
+    @Configuration
+    @ComponentScan("se.inera.intyg.infra.integration.postnummer")
+    @PropertySource(
+            value={"classpath:/test.properties"},
+            ignoreResourceNotFound = false)
+    public static class TestConfiguration {
+        @Bean
+        public static PropertySourcesPlaceholderConfigurer propertiesResolver() {
+            return new PropertySourcesPlaceholderConfigurer();
+        }
+    }
+
+    @Autowired
+    PostnummerService postnummerService;
+
+    @Test
+    public void testGetPostnummer() {
+
+        List<Omrade> omrade13061 = Arrays.asList(new Omrade("13061", "HÅRSFJÄRDEN", "HANINGE", "STOCKHOLM"));
+        List<Omrade> omrade13100 = Arrays.asList(new Omrade("13100", "NACKA", "NACKA", "STOCKHOLM"));
+        List<Omrade> omrade13155 = Arrays.asList(new Omrade("13155", "NACKA", "STOCKHOLM", "STOCKHOLM"),
+                new Omrade("13155", "NACKA", "NACKA", "STOCKHOLM"));
+
+        assertNull(postnummerService.getOmradeByPostnummer(null));
+        assertNull(postnummerService.getOmradeByPostnummer(""));
+        assertNull(postnummerService.getOmradeByPostnummer("xxyy"));
+        assertThat(postnummerService.getOmradeByPostnummer("13061"), is(omrade13061));
+        assertThat(postnummerService.getOmradeByPostnummer("13100"), is(omrade13100));
+        assertThat(postnummerService.getOmradeByPostnummer("13155"), is(omrade13155));
+        assertThat(postnummerService.getOmradeByPostnummer("13155"), not(omrade13061));
+    }
+
+    @Test
+    public void testGetKommunList() {
+        List<String> verify = Arrays.asList("HANINGE", "NACKA", "STOCKHOLM", "VÄSTERVIK", "LINKÖPING");
+
+        assertThat(postnummerService.getKommunList(), is(verify));
+    }
+}
