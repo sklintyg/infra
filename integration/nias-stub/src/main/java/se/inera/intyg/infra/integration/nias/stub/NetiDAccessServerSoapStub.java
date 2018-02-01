cae9e44b7a7970d755798ff0f304a134f4d57653
Erik Lupander <erik.lupander@callistaenterprise.se>
2018-02-01 11:33:03 +0100

    INTYG-5358: Fixade checkstyle, findbugs etc. för WiP-koden för xmldsig och nias-stubbe

--- integration/nias-stub/src/main/java/se/inera/intyg/infra/integration/nias/stub/NetiDAccessServerSoapStub.java
+++ integration/nias-stub/src/main/java/se/inera/intyg/infra/integration/nias/stub/NetiDAccessServerSoapStub.java
@@ -1,32 +1,102 @@
+/*
+ * Copyright (C) 2018 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package se.inera.intyg.infra.integration.nias.stub;
 
 import com.secmaker.netid.nias.v1.AuthenticateResponse;
+import com.secmaker.netid.nias.v1.DeviceInfoType;
 import com.secmaker.netid.nias.v1.NetiDAccessServerSoap;
 import com.secmaker.netid.nias.v1.ResultCollect;
 import com.secmaker.netid.nias.v1.ResultRegister;
+import com.secmaker.netid.nias.v1.SignResponse;
+import com.secmaker.netid.nias.v1.UserInfoType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import se.inera.intyg.infra.integration.nias.stub.model.OngoingSigning;
 
 import javax.xml.bind.JAXB;
 import java.io.StringWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 public class NetiDAccessServerSoapStub implements NetiDAccessServerSoap {
 
     private static final Logger LOG = LoggerFactory.getLogger(NetiDAccessServerSoapStub.class);
+    public static final int NOTBEFORE_OR_AFTER_MINUTES = 5;
+
+    @Autowired
+    private NiasServiceStub niasServiceStub;
 
     @Override
-    public String sign(String s, String s1, String s2, String s3) {
-        return null;
+    public String sign(String personalNumber, String userVisibleData, String userNonVisibleData, String endUserInfo) {
+        String orderRef = UUID.randomUUID().toString();
+        OngoingSigning ongoingSigning = new OngoingSigning(orderRef, personalNumber, userVisibleData, userNonVisibleData, endUserInfo,
+                NiasSignatureStatus.OUTSTANDING_TRANSACTION);
+        niasServiceStub.put(orderRef, ongoingSigning);
+
+        SignResponse signResponse = new SignResponse();
+        signResponse.setSignResult(orderRef);
+
+        StringWriter sw = new StringWriter();
+        JAXB.marshal(signResponse, sw);
+        return sw.toString();
     }
 
     @Override
     public ResultRegister register(String s, String s1, String s2) {
-        return null;
+        throw new IllegalStateException("Not implemented");
     }
 
     @Override
-    public ResultCollect collect(String s) {
-        return null;
+    public ResultCollect collect(String orderRef) {
+        OngoingSigning ongoingSigning = niasServiceStub.get(orderRef);
+
+        ResultCollect resultCollect = new ResultCollect();
+
+        if (ongoingSigning.getStatus() == NiasSignatureStatus.COMPLETE) {
+            resultCollect.setProgressStatus(ongoingSigning.getStatus().name());
+            DeviceInfoType deviceInfoType = new DeviceInfoType();
+            deviceInfoType.setAddress("192.168.1.129");
+            deviceInfoType.setName("OS X");
+            deviceInfoType.setVersion("10.11");
+            resultCollect.setDeviceInfo(deviceInfoType);
+            resultCollect.setSignature("signature-data");
+            UserInfoType userInfoType = new UserInfoType();
+            userInfoType.setGivenName("Tolvan");
+            userInfoType.setName("Tolvan Tolvansson");
+            userInfoType.setPersonalNumber(ongoingSigning.getPersonalNumber());
+            userInfoType.setNotBefore(LocalDateTime.now().minusMinutes(NOTBEFORE_OR_AFTER_MINUTES).format(DateTimeFormatter.ISO_DATE_TIME));
+            userInfoType.setNotAfter(LocalDateTime.now().plusMinutes(NOTBEFORE_OR_AFTER_MINUTES).format(DateTimeFormatter.ISO_DATE_TIME));
+            resultCollect.setUserInfo(userInfoType);
+
+            // Since it's complete, remove
+            niasServiceStub.remove(orderRef);
+        } else if (ongoingSigning.getStatus() == NiasSignatureStatus.USER_SIGN
+                || ongoingSigning.getStatus() == NiasSignatureStatus.OUTSTANDING_TRANSACTION) {
+            resultCollect.setProgressStatus(ongoingSigning.getStatus().name());
+        } else {
+            // Error state, remove signing and return state
+            niasServiceStub.remove(orderRef);
+            resultCollect.setProgressStatus(ongoingSigning.getStatus().name());
+        }
+        return resultCollect;
     }
 
     @Override
--- xmldsig/src/main/java/se/inera/intyg/infra/xmldsig/GenerateXMLDSignature.java
+++ xmldsig/src/main/java/se/inera/intyg/infra/xmldsig/GenerateXMLDSignature.java
@@ -1,20 +1,32 @@
+/*
+ * Copyright (C) 2018 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package se.inera.intyg.infra.xmldsig;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
-import java.util.Collections;
+import org.apache.xml.security.c14n.CanonicalizationException;
+import org.apache.xml.security.c14n.Canonicalizer;
+import org.apache.xml.security.c14n.InvalidCanonicalizerException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import javax.xml.crypto.dsig.CanonicalizationMethod;
 import javax.xml.crypto.dsig.DigestMethod;
 import javax.xml.crypto.dsig.Reference;
-import javax.xml.crypto.dsig.SignedInfo;
 import javax.xml.crypto.dsig.Transform;
 import javax.xml.crypto.dsig.XMLSignatureFactory;
 import javax.xml.crypto.dsig.spec.C14NMethodParameterSpec;
@@ -26,23 +38,25 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-
-import org.jcp.xml.dsig.internal.dom.DOMReference;
-import org.jcp.xml.dsig.internal.dom.DOMUtils;
-import org.jcp.xml.dsig.internal.dom.DOMXMLSignature;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
-
-import com.sun.org.apache.xml.internal.security.c14n.CanonicalizationException;
-import com.sun.org.apache.xml.internal.security.c14n.Canonicalizer;
-import com.sun.org.apache.xml.internal.security.c14n.InvalidCanonicalizerException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Collections;
 
 /**
  * Class for signing and arbitrary XML document with XMLDSig, returning an OutputStream with the resulting
- * DOM including the <Signature></Signature>.
+ * DOM including the <Signature> element.
  *
  * Derived from official Oracle samples
- * https://docs.oracle.com/javase/9/security/java-xml-digital-signature-api-overview-and-tutorial.htm#JSSEC-GUID-E7E9239F-C973-4D05-AC3F-53F714C259DB
+ * https://docs.oracle.com/javase/9/security/java-xml-digital-signature-api-overview-and-tutorial.htm
+ * #JSSEC-GUID-E7E9239F-C973-4D05-AC3F-53F714C259DB
  *
  * @author eriklupander
  */
@@ -58,7 +72,6 @@ public class GenerateXMLDSignature {
         String hash = generateDigest(canonizedXml);
         return hash;
     }
-
 
     public String canon(String str) {
 
@@ -83,7 +96,7 @@ public class GenerateXMLDSignature {
     }
 
     public String generateDigest(String canonXmlString) {
-        //com.sun.org.apache.xml.internal.security.Init.init();
+        // com.sun.org.apache.xml.internal.security.Init.init();
         try {
 
             // get instance of the message digest based on the SHA-256 hashing algorithm
@@ -95,7 +108,7 @@ public class GenerateXMLDSignature {
 
             // encode the endresult byte hash
             byte[] encodedBytes = Base64.getEncoder().encode(hash);
-            return new String(encodedBytes);
+            return new String(encodedBytes, Charset.forName("UTF-8"));
         } catch (NoSuchAlgorithmException e) {
             throw new IllegalArgumentException(e.getMessage());
         }
@@ -113,56 +126,53 @@ public class GenerateXMLDSignature {
             DigestMethod digestMethod = fac.newDigestMethod(DigestMethod.SHA256, null);
 
             Reference ref = fac.newReference("", digestMethod,
-                            Collections.singletonList(fac.newTransform(Transform.ENVELOPED, (TransformParameterSpec) null)),
-                            null, null);
+                    Collections.singletonList(fac.newTransform(Transform.ENVELOPED, (TransformParameterSpec) null)),
+                    null, null);
 
             // Create the SignedInfo
-            SignedInfo si = fac.newSignedInfo
-                    (fac.newCanonicalizationMethod
-                                    (CanonicalizationMethod.INCLUSIVE_WITH_COMMENTS,
-                                            (C14NMethodParameterSpec) null),
-                          //  fac.newSignatureMethod("http://www.w3.org/2000/09/xmldsig#dsa-sha256", null),
-                            fac.newSignatureMethod("http://www.w3.org/2001/04/xmldsig-more#rsa-sha256", null),
-                            Collections.singletonList(ref));
+            // SignedInfo si =
+            fac.newSignedInfo(fac.newCanonicalizationMethod(CanonicalizationMethod.INCLUSIVE_WITH_COMMENTS,
+                    (C14NMethodParameterSpec) null),
+                    // fac.newSignatureMethod("http://www.w3.org/2000/09/xmldsig#dsa-sha256", null),
+                    fac.newSignatureMethod("http://www.w3.org/2001/04/xmldsig-more#rsa-sha256", null),
+                    Collections.singletonList(ref));
 
             // Create a RSA KeyPair
-//            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-//            kpg.initialize(2048);
-//            KeyPair kp = kpg.generateKeyPair();
-//
-//            // Create a KeyValue containing the DSA PublicKey that was generated
-//            KeyInfoFactory kif = fac.getKeyInfoFactory();
-//            KeyValue kv = kif.newKeyValue(kp.getPublic());
-//
-//            // Create a KeyInfo and add the KeyValue to it
-//            KeyInfo ki = kif.newKeyInfo(Collections.singletonList(kv));
+            // KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+            // kpg.initialize(2048);
+            // KeyPair kp = kpg.generateKeyPair();
+            //
+            // // Create a KeyValue containing the DSA PublicKey that was generated
+            // KeyInfoFactory kif = fac.getKeyInfoFactory();
+            // KeyValue kv = kif.newKeyValue(kp.getPublic());
+            //
+            // // Create a KeyInfo and add the KeyValue to it
+            // KeyInfo ki = kif.newKeyInfo(Collections.singletonList(kv));
 
             // Instantiate the document to be signed
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             dbf.setNamespaceAware(true);
-            Document doc =
-                    dbf.newDocumentBuilder().parse(is);
+            Document doc = dbf.newDocumentBuilder().parse(is);
 
             // Create a DOMSignContext and specify the DSA PrivateKey and
             // location of the resulting XMLSignature's parent element
-           // DOMSignContext dsc = new DOMSignContext
-           //         (null, doc.getDocumentElement());
-            DOMPreSignedContext preSignedContext = new DOMPreSignedContext(doc.getDocumentElement());
-
-            DOMReference dref = (DOMReference) ref;
-            dref.digest(preSignedContext);
-
-            //DOMSignContext context = (DOMSignContext)dsc;
-            DOMXMLSignature signature = (DOMXMLSignature) fac.newXMLSignature(si, null);
-            signature.marshal(preSignedContext.getParent(),
-                    DOMUtils.getSignaturePrefix(preSignedContext), preSignedContext);
-
+            // DOMSignContext dsc = new DOMSignContext
+            // (null, doc.getDocumentElement());
+            // // DOMPreSignedContext preSignedContext = new DOMPreSignedContext(doc.getDocumentElement());
+            //
+            // DOMReference dref = (DOMReference) ref;
+            // dref.digest(preSignedContext);
+            //
+            // //DOMSignContext context = (DOMSignContext)dsc;
+            // DOMXMLSignature signature = (DOMXMLSignature) fac.newXMLSignature(si, null);
+            // signature.marshal(preSignedContext.getParent(),
+            // DOMUtils.getSignaturePrefix(preSignedContext), preSignedContext);
 
             // Create the XMLSignature (but don't sign it yet)
-            //XMLSignature signature = fac.newXMLSignature()newXMLSignature(si, ki);
-            //XMLSignature signature = fac.newXMLSignature(si, null, null, "id", "VALUE FROM EFOS");
+            // XMLSignature signature = fac.newXMLSignature()newXMLSignature(si, ki);
+            // XMLSignature signature = fac.newXMLSignature(si, null, null, "id", "VALUE FROM EFOS");
             // Marshal, generate (and sign) the enveloped signature
-            //signature.sign(dsc);
+            // signature.sign(dsc);
 
             // output the resulting document
             OutputStream os = System.out;
