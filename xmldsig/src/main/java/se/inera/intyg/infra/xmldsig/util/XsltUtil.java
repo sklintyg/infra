2267fe32655eba24de3e496cddd74a9f59791dd6
Erik Lupander <erik.lupander@callistaenterprise.se>
2018-06-18 23:22:19 +0200

    DEVENV: Tog bort onödig debug-utskrift

--- xmldsig/src/main/java/se/inera/intyg/infra/xmldsig/util/XsltUtil.java
+++ xmldsig/src/main/java/se/inera/intyg/infra/xmldsig/util/XsltUtil.java
@@ -52,8 +52,6 @@ public final class XsltUtil {
             // Create transformer factory
             TransformerFactory factory = TransformerFactory.newInstance();
 
-            System.out.println("FACTORY: " + factory.getClass().getName());
-
             // Use the factory to create a template containing the xsl file
             Templates template = factory.newTemplates(new StreamSource(
                     cpr.getInputStream()));
