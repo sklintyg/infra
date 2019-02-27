54412435c8928f6dac3e1c897bcbfc3f700c98ea
Joakim Anell <jocke.anell@gmail.com>
2019-02-27 08:19:05 +0100

    BUILDENV: ignored file added

---
+++ buildSrc/src/main/kotlin/se/inera/intyg/infra/build/Properties.kt
@@ -0,0 +1,45 @@
+package se.inera.intyg.infra.build
+
+object Properties {
+    val funktionstjansterGrpSchemasVersion =  "1.0.4"
+
+    val rivtaDirectoryOrganizationSchemasVersion =  "1.1.RC5.5"
+    val rivtaEhrLogstoreSchemasVersion =  "1.2.4"
+    val rivtaHsaAuthorizationmanagementSchemasVersion =  "1.0.RC4.5"
+    val rivtaHsaEmployeeSchemasVersion =  "1.0.RC4.5"
+    val rivtaResidentMasterSchemasVersion =  "1.1.3.5"
+    val rivtaGetPersonsPersonSchemasVersion =  "3.0"
+
+    val intygClinicalprocessSchemasVersion =  "1.0.16"
+
+    val srsSchemasVersion =  "0.0.7"
+
+    val schemasContractVersion =  "2.1.7"
+
+    val springDataRedisVersion =  "1.8.12.RELEASE"
+    val commonsIoVersion =  "2.4"
+    val commonsLangVersion =  "3.8.1"
+    val commonsCollectionsVersion =  "4.2"
+    val cxfVersion =  "3.1.3"
+    val guavaVersion =  "14.0.1"
+    val hamcrestVersion =  "1.3"
+    val igniteSpringVersion =  "1.8.0"
+    val jacksonDataformatVersion =  "2.9.7"
+    val jacksonVersion =  "2.9.7"
+    val logbackVersion =  "1.2.3"
+    val saxonVersion =  "9.8.0-10"
+    val servletApiVersion =  "2.5"
+    val slf4jVersion =  "1.7.21"
+    val springSecuritySaml2CoreVersion =  "1.0.3.RELEASE"
+    val springSecurityVersion =  "4.2.5.RELEASE"
+    val springVersion =  "4.3.17.RELEASE"
+    val xmlSecVersion =  "2.1.1"
+    val prometheusClientVersion =  "0.4.0"
+    val aspectjWeaverVersion =  "1.8.9"
+
+    val awaitilityVersion =  "2.0.0"
+    val mockitoVersion =  "2.16.0"
+    val servletApiTestVersion =  "3.1.0"
+
+    val kotlinVersion = "1.3.21"
+}
