02ed804173b57f0831e363aad279b35ceb7b560b
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2019-05-16 09:49:09 +0200

    INTYGFV-11563 Hämta driftsbanner från intygsadmin och spara i redis (#9)
    * INTYGFV-11563: Flyttat embedded-redis version till variable
    
    * INTYGFV-11563: Service som hämtar banners från intygsadmin
    
    * INTYGFV-11563: Lagt in stub och byt till uuid
    
    * INTYGFV-11563: Kunna sätta uuid och flyttat rest-api till egen xml

---
+++ integration/ia-integration/src/main/java/se/inera/intyg/infra/integration/ia/stub/IAStubRestApi.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2019 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.infra.integration.ia.stub;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+
+import se.inera.intyg.infra.integration.ia.cache.IaCacheConfiguration;
+import se.inera.intyg.infra.integration.ia.model.Banner;
+
+public class IAStubRestApi {
+
+    @Autowired
+    private Cache iaCache;
+
+    @GET
+    @Path("/")
+    public Response getBanners() {
+        List<Banner> banners = queryCache();
+
+        return Response.ok(banners).build();
+    }
+
+    @PUT
+    @Path("/banner")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addBanner(Banner banner) {
+        banner.setId(UUID.randomUUID());
+
+        List<Banner> banners = queryCache();
+        banners.add(banner);
+
+        store(banners.toArray(new Banner[0]));
+
+        return Response.ok().build();
+    }
+
+    private void store(Banner[] banners) {
+        iaCache.put(IaCacheConfiguration.CACHE_KEY, banners);
+    }
+
+    private List<Banner> queryCache() {
+        Banner[] banners = iaCache.get(IaCacheConfiguration.CACHE_KEY, Banner[].class);
+
+        if (banners == null) {
+            return new ArrayList<>();
+        }
+
+        return Arrays.asList(banners);
+    }
+
+    /**
+     * Use to evict all clear all banners.
+     *
+     * @return
+     */
+    @DELETE
+    @Path("/cache")
+    public Response clearCache() {
+        try {
+            iaCache.clear();
+            return Response.ok().build();
+        } catch (Exception e) {
+            return Response.serverError().entity(e.getMessage()).build();
+        }
+    }
+}
---
+++ integration/ia-integration/src/test/java/se/inera/intyg/infra/integration/ia/services/IABannerServiceTest.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (C) 2019 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.infra.integration.ia.services;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.client.ExpectedCount;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import se.inera.intyg.infra.integration.ia.model.Application;
+import se.inera.intyg.infra.integration.ia.model.Banner;
+import se.inera.intyg.infra.integration.ia.model.BannerPriority;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@DirtiesContext
+@ContextConfiguration("classpath:test-context.xml")
+@ActiveProfiles({"test"})
+public class IABannerServiceTest {
+
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    @Autowired
+    private IABannerServiceImpl service;
+
+    @Autowired
+    private Cache iaCache;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private MockRestServiceServer mockServer;
+
+    @BeforeClass
+    public static void init() {
+        mapper.registerModule(new JavaTimeModule()
+                .addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(Banner.FORMAT)))
+                .addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(Banner.FORMAT))));
+    }
+
+    @Before
+    public void setup() {
+        iaCache.clear();
+        mockServer = MockRestServiceServer.createServer(restTemplate);
+    }
+
+    @Test
+    public void testGetBanner() {
+        List<Banner> banners = service.getCurrentBanners();
+
+        assertTrue(banners.isEmpty());
+    }
+
+    @Test
+    public void testLoadBannerEmpty() throws URISyntaxException, JsonProcessingException {
+        List<Banner> banners = new ArrayList<>();
+
+        mockServer.expect(ExpectedCount.once(),
+            requestTo(new URI("http://localhost:8681/actuator/banner/WEBCERT")))
+            .andExpect(method(HttpMethod.GET))
+            .andRespond(withStatus(HttpStatus.OK)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(mapper.writeValueAsString(banners))
+            );
+
+        List<Banner> loadedBanners = service.loadBanners(Application.WEBCERT);
+        mockServer.verify();
+
+        assertEquals(banners.size(), loadedBanners.size());
+    }
+
+    @Test
+    public void testLoadBanners() throws URISyntaxException, JsonProcessingException {
+        LocalDateTime now = LocalDateTime.now();
+        List<Banner> banners = new ArrayList<>();
+
+        Banner banner = new Banner(UUID.randomUUID(), now, Application.WEBCERT, "test msg", now.minusDays(10), now.plusDays(10), BannerPriority.HIGH);
+        banners.add(banner);
+
+        mockServer.expect(ExpectedCount.once(),
+                requestTo(new URI("http://localhost:8681/actuator/banner/WEBCERT")))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withStatus(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(mapper.writeValueAsString(banners))
+                );
+
+        List<Banner> loadedBanners = service.loadBanners(Application.WEBCERT);
+        mockServer.verify();
+
+        assertEquals(banners.size(), loadedBanners.size());
+
+        List<Banner> bannersFromStore = service.getCurrentBanners();
+
+        assertEquals(1, bannersFromStore.size());
+    }
+}
