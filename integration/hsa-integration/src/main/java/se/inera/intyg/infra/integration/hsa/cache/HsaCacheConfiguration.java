e21cce58a0162eff0b649d16ecbaee04cb07ad0e
Daniel Petersson <daniel.pete@gmail.com>
2019-08-08 16:40:19 +0200

    INTYGFV-12049: Lagt till cache på HSA employee

--- integration/hsa-integration/src/main/java/se/inera/intyg/infra/integration/hsa/cache/HsaCacheConfiguration.java
+++ integration/hsa-integration/src/main/java/se/inera/intyg/infra/integration/hsa/cache/HsaCacheConfiguration.java
@@ -18,9 +18,11 @@
  */
 package se.inera.intyg.infra.integration.hsa.cache;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.Collection;
 import java.util.Collections;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.Cache;
@@ -28,82 +30,102 @@ import org.springframework.cache.interceptor.CacheOperationInvocationContext;
 import org.springframework.cache.interceptor.CacheResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import se.inera.intyg.infra.rediscache.core.RedisCacheOptionsSetter;
 import se.riv.infrastructure.directory.organization.gethealthcareunitmembersresponder.v1.HealthCareUnitMembersType;
 import se.riv.infrastructure.directory.organization.gethealthcareunitresponder.v1.HealthCareUnitType;
 import se.riv.infrastructure.directory.organization.getunitresponder.v1.UnitType;
+import se.riv.infrastructure.directory.v1.PersonInformationType;
 
 /**
- * While the cacheManager.getCache(...) isn't strictly necessary for creating the cache used by
- * {@link se.inera.intyg.infra.integration.hsa.client.OrganizationUnitService}, this class provides us with the capability
- * of configuring individual caches based on the current state of the (dynamic) configuration
+ * While the cacheManager.getCache(...) isn't strictly necessary for creating the cache used by {@link
+ * se.inera.intyg.infra.integration.hsa.client.OrganizationUnitService}, this class provides us with the capability of configuring
+ * individual caches based on the current state of the (dynamic) configuration
  * <p>
  * Created by eriklupander on 2016-10-20.
  */
 @Configuration
 public class HsaCacheConfiguration {
 
-    static final String HSA_UNIT_CACHE_PREFIX = "hsaUnitCache:";
-    static final String HSA_HEALTH_CARE_UNIT_CACHE_PREFIX = "hsaHealthCareUnitCache:";
-    static final String HSA_HEALTH_CARE_UNIT_MEMBERS_CACHE_PREFIX = "hsaHealthCareUnitMembersCache:";
+  static final String HSA_UNIT_CACHE_PREFIX = "hsaUnitCache:";
+  static final String HSA_HEALTH_CARE_UNIT_CACHE_PREFIX = "hsaHealthCareUnitCache:";
+  static final String HSA_HEALTH_CARE_UNIT_MEMBERS_CACHE_PREFIX = "hsaHealthCareUnitMembersCache:";
+  static final String HSA_EMPLOYEE_CACHE_PREFIX = "hsaEmployeeCache:";
 
-    private static final String HSA_UNIT_CACHE_EXPIRY = "hsa.unit.cache.expiry";
-    private static final String HSA_HEALTHCAREUNIT_CACHE_EXPIRY = "hsa.healthcareunit.cache.expiry";
-    private static final String HSA_HEALHCAREUNITMEMBERS_CACHE_EXPIRY = "hsa.healhcareunitmembers.cache.expiry";
+  private static final String HSA_UNIT_CACHE_EXPIRY = "hsa.unit.cache.expiry";
+  private static final String HSA_HEALTHCAREUNIT_CACHE_EXPIRY = "hsa.healthcareunit.cache.expiry";
+  private static final String HSA_HEALHCAREUNITMEMBERS_CACHE_EXPIRY = "hsa.healhcareunitmembers.cache.expiry";
+  private static final String HSA_EMPLOYEE_CACHE_EXPIRY = "hsa.employee.cache.expiry";
 
-    @Value("${" + HSA_UNIT_CACHE_EXPIRY + "}")
-    private String hsaUnitCacheExpirySeconds;
+  @Value("${" + HSA_UNIT_CACHE_EXPIRY + "}")
+  private String hsaUnitCacheExpirySeconds;
 
-    @Value("${" + HSA_HEALTHCAREUNIT_CACHE_EXPIRY + "}")
-    private String hsaHealthCareUnitCacheExpirySeconds;
+  @Value("${" + HSA_HEALTHCAREUNIT_CACHE_EXPIRY + "}")
+  private String hsaHealthCareUnitCacheExpirySeconds;
 
-    @Value("${" + HSA_HEALHCAREUNITMEMBERS_CACHE_EXPIRY + "}")
-    private String hsaHeathCareUnitMembersCacheExpirySeconds;
+  @Value("${" + HSA_HEALHCAREUNITMEMBERS_CACHE_EXPIRY + "}")
+  private String hsaHeathCareUnitMembersCacheExpirySeconds;
 
-    @Value("${app.name:noname}")
-    private String appName;
+  @Value("${" + HSA_EMPLOYEE_CACHE_EXPIRY + ":60}")
+  private String hsaEmployeeCacheExpirySeconds;
+
+  @Value("${app.name:noname}")
+  private String appName;
 
 
-    @Autowired
-    private RedisCacheOptionsSetter redisCacheOptionsSetter;
+  @Autowired
+  private RedisCacheOptionsSetter redisCacheOptionsSetter;
 
-    @Bean
-    Cache hsaUnitCache() {
-        return newHsaCache(HSA_UNIT_CACHE_PREFIX, hsaUnitCacheExpirySeconds);
-    }
+  @Bean
+  Cache hsaUnitCache() {
+    return newHsaCache(HSA_UNIT_CACHE_PREFIX, hsaUnitCacheExpirySeconds);
+  }
 
-    @Bean
-    Cache hsaCareUnitCache() {
-        return newHsaCache(HSA_HEALTH_CARE_UNIT_CACHE_PREFIX, hsaHealthCareUnitCacheExpirySeconds);
-    }
+  @Bean
+  Cache hsaCareUnitCache() {
+    return newHsaCache(HSA_HEALTH_CARE_UNIT_CACHE_PREFIX, hsaHealthCareUnitCacheExpirySeconds);
+  }
 
-    @Bean
-    Cache hsaCareUnitMemberCache() {
-        return newHsaCache(HSA_HEALTH_CARE_UNIT_MEMBERS_CACHE_PREFIX, hsaHeathCareUnitMembersCacheExpirySeconds);
-    }
+  @Bean
+  Cache hsaCareUnitMemberCache() {
+    return newHsaCache(HSA_HEALTH_CARE_UNIT_MEMBERS_CACHE_PREFIX, hsaHeathCareUnitMembersCacheExpirySeconds);
+  }
 
-    Cache newHsaCache(String prefix, String expiry) {
-        return redisCacheOptionsSetter.createCache(prefix + appName, expiry);
-    }
+  @Bean
+  Cache hsaEmployeeCache() {
+    return newHsaCache(HSA_EMPLOYEE_CACHE_PREFIX, hsaEmployeeCacheExpirySeconds);
+  }
 
-    @Bean
-    CacheResolver hsaCacheResolver() {
-        return new CacheResolver() {
-            @Override
-            public Collection<? extends Cache> resolveCaches(CacheOperationInvocationContext<?> context) {
-                if (context.getMethod().getReturnType() == UnitType.class) {
-                    return Collections.singleton(hsaUnitCache());
-                }
-                if (context.getMethod().getReturnType() == HealthCareUnitType.class) {
-                    return Collections.singleton(hsaCareUnitCache());
-                }
-                if (context.getMethod().getReturnType() == HealthCareUnitMembersType.class) {
-                    return Collections.singleton(hsaCareUnitMemberCache());
-                }
+  private Cache newHsaCache(String prefix, String expiry) {
+    return redisCacheOptionsSetter.createCache(prefix + appName, expiry);
+  }
 
-                return Collections.emptyList();
+  @Bean
+  CacheResolver hsaCacheResolver() {
+    return new CacheResolver() {
+      @Override
+      public Collection<? extends Cache> resolveCaches(CacheOperationInvocationContext<?> context) {
+        if (context.getMethod().getReturnType() == UnitType.class) {
+          return Collections.singleton(hsaUnitCache());
+        }
+        if (context.getMethod().getReturnType() == HealthCareUnitType.class) {
+          return Collections.singleton(hsaCareUnitCache());
+        }
+        if (context.getMethod().getReturnType() == HealthCareUnitMembersType.class) {
+          return Collections.singleton(hsaCareUnitMemberCache());
+        }
+
+        if (context.getMethod().getReturnType() == List.class) {
+          ParameterizedType parameterizedReturnType = (ParameterizedType) context.getMethod().getGenericReturnType();
+          Type[] actualTypeArguments = parameterizedReturnType.getActualTypeArguments();
+          if (actualTypeArguments.length == 1) {
+            if (actualTypeArguments[0] == PersonInformationType.class) {
+              return Collections.singleton(hsaEmployeeCache());
             }
-        };
-    }
+          }
+        }
+
+        return Collections.emptyList();
+      }
+    };
+  }
 }
