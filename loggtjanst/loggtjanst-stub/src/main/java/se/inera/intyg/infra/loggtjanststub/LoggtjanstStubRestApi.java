ea75ea8a667afabb4f65c1dbf7b9d6a7e51f327f
Mikael Heglind <mikael.heglind@soprasteria.com>
2020-05-06 14:50:36 +0200

    INTYGFV-13147: Change to Spring rest in loggtjanst-stub.

--- loggtjanst/loggtjanst-stub/src/main/java/se/inera/intyg/infra/loggtjanststub/LoggtjanstStubRestApi.java
+++ loggtjanst/loggtjanst-stub/src/main/java/se/inera/intyg/infra/loggtjanststub/LoggtjanstStubRestApi.java
@@ -19,19 +19,22 @@
 package se.inera.intyg.infra.loggtjanststub;
 
 import java.util.Collection;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import se.riv.informationsecurity.auditing.log.v2.LogType;
 
 /**
  * @author eriklup
  */
+@RestController
+@RequestMapping("/loggtjanst-api")
 public class LoggtjanstStubRestApi {
 
     @Autowired
@@ -40,30 +43,26 @@ public class LoggtjanstStubRestApi {
     @Autowired
     private StubState stubState;
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public Collection<LogType> getAllLogEntries() {
         return logStore.getAll();
     }
 
-    @DELETE
-    public Response deleteLogStore() {
+    @DeleteMapping
+    public void deleteLogStore() {
         logStore.clear();
-        return Response.ok().build();
     }
 
-    @GET
-    @Path("/online")
-    public Response activateStub() {
+    @GetMapping(path = "/online")
+    public ResponseEntity<String> activateStub() {
         stubState.setActive(true);
-        return Response.ok().entity("OK").build();
+        return ResponseEntity.ok("OK");
     }
 
-    @GET
-    @Path("/offline")
-    public Response deactivateStub() {
+    @GetMapping("/offline")
+    public ResponseEntity<String> deactivateStub() {
         stubState.setActive(false);
-        return Response.ok().entity("OK").build();
+        return ResponseEntity.ok("OK");
     }
 
     /**
@@ -73,16 +72,16 @@ public class LoggtjanstStubRestApi {
      * @return 200 OK if state change was successful. 500 Server Error if the errorType string couldn't be parsed into
      * an {@link ErrorState}
      */
-    @GET
-    @Path("/error/{errorType}")
-    public Response activateErrorState(@PathParam("errorType") String errorType) {
+    @GetMapping(path = "/error/{errorType}")
+    public ResponseEntity<String> activateErrorState(@PathVariable String errorType) {
         try {
             ErrorState errorState = ErrorState.valueOf(errorType);
             stubState.setErrorState(errorState);
-            return Response.ok().entity("OK").build();
+            return ResponseEntity.ok("OK");
         } catch (IllegalArgumentException e) {
-            return Response.serverError().entity("Unknown ErrorState: " + errorType + ". Allowed values are NONE, ERROR, VALIDATION")
-                .build();
+            return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Unknown ErrorState: \" + errorType + \". Allowed values are NONE, ERROR, VALIDATION");
         }
     }
 
@@ -92,10 +91,9 @@ public class LoggtjanstStubRestApi {
      * @param latencyMillis Latency, in milliseconds.
      * @return 200 OK
      */
-    @GET
-    @Path("/latency/{latencyMillis}")
-    public Response setLatency(@PathParam("latencyMillis") Long latencyMillis) {
+    @GetMapping(path = "/latency/{latencyMillis}")
+    public ResponseEntity<String> setLatency(@PathVariable Long latencyMillis) {
         stubState.setArtificialLatency(latencyMillis);
-        return Response.ok().entity("OK").build();
+        return ResponseEntity.ok("OK");
     }
 }
