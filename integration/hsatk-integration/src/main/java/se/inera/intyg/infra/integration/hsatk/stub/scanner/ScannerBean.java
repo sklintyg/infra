2d04960f17381ace9c7d295585e2621d4a963c2c
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2021-05-11 07:32:10 +0200

    INTYGFV-13970: Add functionality to hsa-tk stub to dynamically load hsa persons and care providers from file. The implementation was copied from hsa-integration and adjusted for the new hsatk-integration module.

---
+++ integration/hsatk-integration/src/main/java/se/inera/intyg/infra/integration/hsatk/stub/scanner/ScannerBean.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (C) 2021 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.inera.intyg.infra.integration.hsatk.stub.scanner;
+
+import com.sun.nio.file.SensitivityWatchEventModifier;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchService;
+import java.util.stream.Stream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by eriklupander on 2017-04-12.
+ */
+@Service
+@Profile({"dev", "wc-hsa-stub", "wc-all-stubs"})
+public class ScannerBean {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ScannerBean.class);
+
+    private static final String SUFFIX_JSON = ".json";
+    private static final String ENTRY_CREATE = "ENTRY_CREATE";
+    private static final String ENTRY_MODIFY = "ENTRY_MODIFY";
+    private static final String ENTRY_DELETE = "ENTRY_DELETE";
+
+    private final WatchEventPersonHandler personHandler;
+    private final WatchEventCareProviderHandler careProviderHandler;
+
+    @Autowired
+    public ScannerBean(WatchEventPersonHandler personHandler, WatchEventCareProviderHandler careProviderHandler) {
+        this.personHandler = personHandler;
+        this.careProviderHandler = careProviderHandler;
+    }
+
+    public void bootstrapScan(Path path, ScanTarget scanTarget) {
+        LOG.info("Performing startup scan of HSA identity folder '{}'", path.toString());
+        final var handler = resolveHandler(scanTarget);
+
+        try (Stream<Path> stream = Files.walk(path)) {
+            stream.filter(Files::isRegularFile)
+                .filter(p -> p.toString().endsWith(SUFFIX_JSON))
+                .forEach(handler::created);
+        } catch (IOException e) {
+            LOG.error("Initial scan of " + path + " failed: " + e.getMessage());
+        }
+    }
+
+    @Async
+    public void scan(Path path, ScanTarget scanTarget) {
+
+        LOG.info("Starting WatchService for folder: " + path.toString());
+
+        final var isFolder = Files.isDirectory(path);
+        if (!isFolder) {
+            throw new IllegalArgumentException("Path: " + path + " is not a folder");
+        }
+
+        final var fs = path.getFileSystem();
+
+        try (WatchService service = fs.newWatchService()) {
+            path.register(service,
+                new WatchEvent.Kind[]{
+                    StandardWatchEventKinds.ENTRY_CREATE,
+                    StandardWatchEventKinds.ENTRY_MODIFY,
+                    StandardWatchEventKinds.ENTRY_DELETE
+                },
+                SensitivityWatchEventModifier.HIGH);
+
+            pollForEvents(path, service, scanTarget);
+
+        } catch (IOException | InterruptedException ioe) {
+            throw new IllegalStateException(ioe.getMessage());
+        }
+    }
+
+    private void pollForEvents(Path path, WatchService service, ScanTarget scanTarget) throws InterruptedException {
+        ScanEventHandler handler = resolveHandler(scanTarget);
+        while (true) {
+            final var key = service.take();
+            for (WatchEvent<?> watchEvent : key.pollEvents()) {
+
+                final var name = path.resolve((Path) watchEvent.context());
+
+                if (!name.toString().endsWith(SUFFIX_JSON)) {
+                    continue;
+                }
+
+                switch (watchEvent.kind().name()) {
+                    case ENTRY_CREATE:
+                        handler.created(name);
+                        break;
+                    case ENTRY_MODIFY:
+                        handler.modified(name);
+                        break;
+                    case ENTRY_DELETE:
+                        handler.deleted(name);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if (!key.reset()) {
+                break;
+            }
+        }
+    }
+
+    private ScanEventHandler resolveHandler(ScanTarget scanTarget) {
+        return scanTarget == ScanTarget.CARE_PROVIDER ? careProviderHandler : personHandler;
+    }
+}
