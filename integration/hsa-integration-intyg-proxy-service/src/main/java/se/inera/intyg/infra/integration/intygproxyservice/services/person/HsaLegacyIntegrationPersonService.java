46ca2335d277d281358e13e245335fda1d341842
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-11-29 10:23:29 +0100

    INTYGFV-16143: Implemented legacy get hsa person info

---
+++ integration/hsa-integration-intyg-proxy-service/src/main/java/se/inera/intyg/infra/integration/intygproxyservice/services/person/HsaLegacyIntegrationPersonService.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2023 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.infra.integration.intygproxyservice.services.person;
+
+import static se.inera.intyg.infra.integration.hsatk.constants.HsaIntegrationApiConstants.HSA_INTEGRATION_INTYG_PROXY_SERVICE_PROFILE;
+
+import java.util.List;
+import javax.xml.ws.WebServiceException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import se.inera.intyg.infra.integration.hsatk.exception.HsaServiceCallException;
+import se.inera.intyg.infra.integration.hsatk.model.Commission;
+import se.inera.intyg.infra.integration.hsatk.model.PersonInformation;
+import se.inera.intyg.infra.integration.hsatk.services.legacy.HsaPersonService;
+import se.inera.intyg.infra.integration.intygproxyservice.dto.employee.GetEmployeeRequestDTO;
+import se.inera.intyg.infra.integration.intygproxyservice.services.employee.GetEmployeeService;
+
+@Slf4j
+@Service
+@Profile(HSA_INTEGRATION_INTYG_PROXY_SERVICE_PROFILE)
+@RequiredArgsConstructor
+public class HsaLegacyIntegrationPersonService implements HsaPersonService {
+
+    private final GetEmployeeService getEmployeeService;
+
+    @Override
+    public List<PersonInformation> getHsaPersonInfo(String personHsaId) {
+        try {
+            return getEmployeeService.get(
+                GetEmployeeRequestDTO.builder()
+                    .hsaId(personHsaId)
+                    .build()
+            );
+        } catch (HsaServiceCallException e) {
+            throw new WebServiceException(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<Commission> checkIfPersonHasMIUsOnUnit(String hosPersonHsaId, String unitHsaId) throws HsaServiceCallException {
+        return null;
+    }
+}
