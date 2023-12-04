8a375eb2e5631697f136858d2c0a87ab42cce738
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-12-04 10:10:23 +0100

    INTYGFV-16172: Updated profiles.

--- integration/hsa-integration-api/src/main/java/se/inera/intyg/infra/integration/hsatk/constants/HsaIntegrationApiConstants.java
+++ integration/hsa-integration-api/src/main/java/se/inera/intyg/infra/integration/hsatk/constants/HsaIntegrationApiConstants.java
@@ -26,7 +26,5 @@ public class HsaIntegrationApiConstants {
     }
 
     public static final String HSA_INTEGRATION_INTYG_PROXY_SERVICE_PROFILE = "hsa-integration-intyg-proxy-service";
-    public static final String HSA_INTEGRATION_INTYG_PROXY_SERVICE_NOT_ACTIVATED_PROFILE =
-        "hsa-integration-intyg-proxy-service-not-activated";
 
 }
