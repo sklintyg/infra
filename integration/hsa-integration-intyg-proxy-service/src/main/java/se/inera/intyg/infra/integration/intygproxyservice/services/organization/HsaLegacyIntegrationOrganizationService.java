650898aad69aace9712044c2d57baed56c689a3d
Michaela Hörnfeldt <54178725+mhornfeldt@users.noreply.github.com>
2023-12-04 10:11:58 +0100

    INTYGFV-16158: Implemented getAuthorizedEnheterForHosPerson (#224)

--- integration/hsa-integration-intyg-proxy-service/src/main/java/se/inera/intyg/infra/integration/intygproxyservice/services/organization/HsaLegacyIntegrationOrganizationService.java
+++ integration/hsa-integration-intyg-proxy-service/src/main/java/se/inera/intyg/infra/integration/intygproxyservice/services/organization/HsaLegacyIntegrationOrganizationService.java
@@ -32,9 +32,11 @@ import se.inera.intyg.infra.integration.hsatk.model.legacy.UserAuthorizationInfo
 import se.inera.intyg.infra.integration.hsatk.model.legacy.Vardenhet;
 import se.inera.intyg.infra.integration.hsatk.model.legacy.Vardgivare;
 import se.inera.intyg.infra.integration.hsatk.services.legacy.HsaOrganizationsService;
+import se.inera.intyg.infra.integration.intygproxyservice.dto.authorization.GetCredentialInformationRequestDTO;
 import se.inera.intyg.infra.integration.intygproxyservice.dto.organization.GetHealthCareUnitMembersRequestDTO;
 import se.inera.intyg.infra.integration.intygproxyservice.dto.organization.GetHealthCareUnitRequestDTO;
 import se.inera.intyg.infra.integration.intygproxyservice.dto.organization.GetUnitRequestDTO;
+import se.inera.intyg.infra.integration.intygproxyservice.services.authorization.GetCredentialInformationForPersonService;
 
 @Slf4j
 @Service
@@ -45,11 +47,18 @@ public class HsaLegacyIntegrationOrganizationService implements HsaOrganizations
     private final GetActiveHealthCareUnitMemberHsaIdService getActiveHealthCareUnitMemberHsaIdService;
     private final GetHealthCareUnitService getHealthCareUnitService;
     private final GetUnitService getUnitService;
+    private final GetCredentialInformationForPersonService getCredentialInformationForPersonService;
+    private final GetUserAuthorizationInfoService getUserAuthorizationInfoService;
 
     @Override
     public UserAuthorizationInfo getAuthorizedEnheterForHosPerson(String hosPersonHsaId) {
-        log.info("work in progress");
-        return null;
+        final var credentialInformation = getCredentialInformationForPersonService.get(
+            GetCredentialInformationRequestDTO.builder()
+                .personHsaId(hosPersonHsaId)
+                .build()
+        );
+
+        return getUserAuthorizationInfoService.get(credentialInformation);
     }
 
     @Override
